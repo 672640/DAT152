05142cf713a4c7a1248ff63a4b6240b115b8e72c
Joachim Øen <joachimoen02@gmail.com>
2025-09-20 01:45:53 +0200

    BS_Testing

---
+++ Ikkje_relevant/Lab2a/library-flowmanager/src/main/java/no/hvl/dat152/controller/FlowManager.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package no.hvl.dat152.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 
+ */
+public class FlowManager {
+
+	private final Map<String, String> pages;
+	/**
+	 * 
+	 */
+	public FlowManager() {
+		
+		this.pages = new HashMap<>();
+		
+		pages.put("addbookform", "/addbook.jsp");
+		pages.put("addbook", "/viewbooks.jsp");
+		pages.put("updatebookform", "/updatebook.jsp");
+		pages.put("updatebook", "/viewbook.jsp");
+		pages.put("viewbook", "/viewbook.jsp");
+		pages.put("viewbooks", "/viewbooks.jsp");
+		// new features
+		pages.put("addauthorform", "/addauthor.jsp");				//TODO
+		pages.put("addauthor", "/viewbooks.jsp");
+		pages.put("deletebookconfirm", "/viewbooks.jsp");			//TODO
+		pages.put("deletebook", "/viewbooks.jsp");
+		pages.put("deletebookaction", "/viewbooks.jsp");
+	}
+	
+	public String getPage(String cmd) {
+		return pages.get(cmd);
+	}
+
+}
---
+++ Ikkje_relevant/Lab2b/library-flowmanager-auth/src/main/java/no/hvl/dat152/controller/FlowManager.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package no.hvl.dat152.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 
+ */
+public class FlowManager {
+
+	private final Map<String, String> pages;
+	/**
+	 * 
+	 */
+	public FlowManager() {
+		
+		this.pages = new HashMap<>();
+		
+		pages.put("addbookform", "/addbook.jsp");
+		pages.put("addbook", "/viewbooks.jsp");
+		pages.put("updatebookform", "/updatebook.jsp");
+		pages.put("updatebook", "/viewbook.jsp");
+		pages.put("viewbook", "/viewbook.jsp");
+		pages.put("viewbooks", "/viewbooks.jsp");
+		pages.put("loginform", "/login.jsp");
+		pages.put("addauthorform", "/addauthor.jsp");
+		pages.put("addauthor", "/viewbooks.jsp");
+		pages.put("deletebookconfirm", "/viewbooks.jsp");
+		pages.put("deletebook", "/viewbooks.jsp");
+		pages.put("deletebookaction", "/viewbooks.jsp");
+	}
+	
+	public String getPage(String cmd) {
+		return pages.get(cmd);
+	}
+
+}
